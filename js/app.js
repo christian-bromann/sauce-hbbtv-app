482374ab7f18c51d25d78fd00f0dd8f538b4164e
christian-bromann <mail@christian-bromann.com>
2017-10-06 14:33:59 +0200

    add basic app

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/*
+ * Institut für Rundfunktechnik (IRT | www.irt.de) Germany, 2014
+ * Demo of simple HbbTV Application realised within the EU founded TV-Ring project.
+ * contact: contact@hbbtv-developer.com
+ */
+var app_area;
+var indicator;
+
+function initApp() { 
+    app_area = document.getElementById("app_area");
+    indicator = document.getElementById("indicator");
+
+    app_area.style.visibility = "visible";
+    
+    var application_manager = document.getElementById("appMan");
+    var Application = application_manager.getOwnerApplication(document)
+    Application.show();
+    
+    document.addEventListener("keydown", function (e) {
+        console.log('KeyEvent:', e.keyCode);
+		if(handleKeyCode(e.keyCode)) e.preventDefault();
+	}, false);
+}
+
+function handleKeyCode(kc) {
+    switch (kc) {
+        case VK_RED:
+            showApp();
+            return true;
+            break;
+        default:
+            return false;
+    }
+}
+
+function showApp() {
+    indicator.style.visibility = "hidden";
+    app_area.style.visibility = "visible";
+}
